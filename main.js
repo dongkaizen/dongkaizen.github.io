c4d6ea439f84ce947d4581c22a20ba19b57cc60b
dongkaizen <takemassiveaction007@gmail.com>
2020-09-09 19:29:59 +0700

    truyen vao message

---
+++ main.js
@@ -0,0 +1,1 @@
+console.log("Xin chao cac ban")
---
+++ onha2.js
@@ -0,0 +1,10 @@
+<html> 
+    <body> 
+        <p id="GFG"></p> 
+        <script> 
+            var str = "Welcome to GeeksforGeeks."; 
+            var check = str.includes("Geeks"); 
+            document.getElementById("GFG").innerHTML = check; 
+        </script> 
+    </body> 
+</html> 
